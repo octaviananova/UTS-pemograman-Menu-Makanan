ae80e07360fbf0472bbc99cb676fd666bbae25fe
dedykaryadigunawan <octaviananova729@gmail.com>
2020-11-30 17:59:59 +0700

    Menu Makanan

---
+++ app/src/main/java/com/nova/menurumahmakan/makanan/Minuman.java
@@ -0,0 +1,10 @@
+package com.nova.menurumahmakan.makanan;
+
+public class Minuman extends Menu{
+
+    public Minuman(String makanan, String harga, String deskripsi, int drawableRes) {
+        super("Minuman",makanan,harga,deskripsi,drawableRes);
+
+    }
+
+}
